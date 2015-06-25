5f26a7828c4059336a59a3991b70bea70c784273
Steve Elliott <steve@tegud.net>
2015-06-25 22:11:55 +0100

    Session RatesAccuracySummary

--- test/ComposedObjectFactories/session/buildRatesAccuracySummary.js
+++ test/ComposedObjectFactories/session/buildRatesAccuracySummary.js
@@ -10,4 +10,34 @@ describe('buildRatesAccuracySummary', function() {
 			]
 		}).rateAccuracyResults).to.be(2);
 	});
+
+	it('sets the outcomes to space delimited list of rules', function() {
+		expect(buildSummary({ 
+			events: [
+				{ type: 'rates_accuracy_result', result: 'OK' },
+				{ type: 'rates_accuracy_result', result: 'OK' },
+				{ type: 'rates_accuracy_result', result: 'NO_AVAILABILITY' }
+			]
+		}).outcomes).to.be('OK OK NO_AVAILABILITY');
+	});
+
+	it('sets the outcomes to space delimited list of rules', function() {
+		expect(buildSummary({ 
+			events: [
+				{ type: 'rates_accuracy_result', result: 'OK' },
+				{ type: 'rates_accuracy_result', result: 'OK' },
+				{ type: 'rates_accuracy_result', result: 'NO_AVAILABILITY' }
+			]
+		}).outcomes).to.be('OK OK NO_AVAILABILITY');
+	});
+
+	it('sets the failures', function() {
+		expect(buildSummary({ 
+			events: [
+				{ type: 'rates_accuracy_result', result: 'OK' },
+				{ type: 'rates_accuracy_result', result: 'OK' },
+				{ type: 'rates_accuracy_result', result: 'NO_AVAILABILITY' }
+			]
+		}).failures).to.be(1);
+	});	
 });
