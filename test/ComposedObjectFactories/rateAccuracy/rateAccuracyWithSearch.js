1a10485e61bb4b3ddbfb31c00f9ec49c98ed9381
Steve Elliott <steve@tegud.net>
2015-07-05 19:32:16 +0100

    Deal with postcode keywords

--- test/ComposedObjectFactories/rateAccuracy/rateAccuracyWithSearch.js
+++ test/ComposedObjectFactories/rateAccuracy/rateAccuracyWithSearch.js
@@ -65,90 +65,49 @@ describe('ratesAccuracyCheck with search', function() {
 		});
 	});
 
-	it('sets search keyword', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
+	describe('keyword', function() {
 
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.keyword).to.eql('luton');
-			done();
-		});
-	});
-
-	it('sets search type to keyword', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
-
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.type).to.eql('keyword');
-			done();
-		});
-	});
-
-	it('sets search keywordId', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
-
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.keywordId).to.eql(16296071);
-			done();
-		});
-	});
-
-	it('sets search two word keyword', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
-
-		parsedData[1].url_page = '/k16295577_loch-lomond-hotels.aspx';
-
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.keyword).to.eql('loch lomond');
-			done();
-		});
-	});
-
-	it('with no page number sets pageNumber to first', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
-
-		parsedData[1].url_page = '/k16295577_loch-lomond-hotels.aspx';
-
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.pageNumber).to.eql(1);
-			done();
-		});
-	});
-
-	it('with page number sets pageNumber', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
-
-		parsedData[1].url_page = '/k16295577_loch-lomond-hotels-p2.aspx';
-
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.pageNumber).to.eql(2);
-			done();
-		});
-	});
-
-	describe('quick search keyword search', function() {
-		it('sets keyword', function(done) {
+		it('sets search keyword', function(done) {
 			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
 			var parsedData = JSON.parse(fileData);
 
-			parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond.aspx';
+			buildRequest({
+				events: parsedData
+			}).then(function(result) {
+				expect(result[0].search.keyword).to.eql('luton');
+				done();
+			});
+		});
+
+		it('sets search type to keyword', function(done) {
+			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+			var parsedData = JSON.parse(fileData);
+
+			buildRequest({
+				events: parsedData
+			}).then(function(result) {
+				expect(result[0].search.type).to.eql('keyword');
+				done();
+			});
+		});
+
+		it('sets search keywordId', function(done) {
+			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+			var parsedData = JSON.parse(fileData);
+
+			buildRequest({
+				events: parsedData
+			}).then(function(result) {
+				expect(result[0].search.keywordId).to.eql(16296071);
+				done();
+			});
+		});
+
+		it('sets search two word keyword', function(done) {
+			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+			var parsedData = JSON.parse(fileData);
+
+			parsedData[1].url_page = '/k16295577_loch-lomond-hotels.aspx';
 
 			buildRequest({
 				events: parsedData
@@ -158,44 +117,16 @@ describe('ratesAccuracyCheck with search', function() {
 			});
 		});
 
-		it('sets keywordId', function(done) {
+		it('sets search postcode keyword', function(done) {
 			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
 			var parsedData = JSON.parse(fileData);
 
-			parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond.aspx';
+			parsedData[1].url_page = '/k17392527_se10-0dx-hotels.aspx';
 
 			buildRequest({
 				events: parsedData
 			}).then(function(result) {
-				expect(result[0].search.keywordId).to.eql(16295577);
-				done();
-			});
-		});
-
-		it('sets quickSearchType', function(done) {
-			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-			var parsedData = JSON.parse(fileData);
-
-			parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond.aspx';
-
-			buildRequest({
-				events: parsedData
-			}).then(function(result) {
-				expect(result[0].search.quickSearchType).to.eql('luxury hotels');
-				done();
-			});
-		});
-
-		it('sets isQuickSearch', function(done) {
-			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-			var parsedData = JSON.parse(fileData);
-
-			parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond.aspx';
-
-			buildRequest({
-				events: parsedData
-			}).then(function(result) {
-				expect(result[0].search.isQuickSearch).to.eql(true);
+				expect(result[0].search.keyword).to.eql('se10 0dx');
 				done();
 			});
 		});
@@ -204,7 +135,7 @@ describe('ratesAccuracyCheck with search', function() {
 			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
 			var parsedData = JSON.parse(fileData);
 
-			parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond.aspx';
+			parsedData[1].url_page = '/k16295577_loch-lomond-hotels.aspx';
 
 			buildRequest({
 				events: parsedData
@@ -218,7 +149,7 @@ describe('ratesAccuracyCheck with search', function() {
 			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
 			var parsedData = JSON.parse(fileData);
 
-			parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond-p2.aspx';
+			parsedData[1].url_page = '/k16295577_loch-lomond-hotels-p2.aspx';
 
 			buildRequest({
 				events: parsedData
@@ -227,145 +158,147 @@ describe('ratesAccuracyCheck with search', function() {
 				done();
 			});
 		});
-	});
 
-	it('sets search region', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
+		describe('quick search keyword search', function() {
+			it('sets keyword', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
 
-		parsedData[1].url_page = '/r11550_hotels-in-glasgow.aspx';
+				parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond.aspx';
 
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.region).to.eql('glasgow');
-			done();
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.keyword).to.eql('loch lomond');
+					done();
+				});
+			});
+
+			it('sets keywordId', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
+
+				parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond.aspx';
+
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.keywordId).to.eql(16295577);
+					done();
+				});
+			});
+
+			it('sets quickSearchType', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
+
+				parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond.aspx';
+
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.quickSearchType).to.eql('luxury hotels');
+					done();
+				});
+			});
+
+			it('sets isQuickSearch', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
+
+				parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond.aspx';
+
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.isQuickSearch).to.eql(true);
+					done();
+				});
+			});
+
+			it('with no page number sets pageNumber to first', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
+
+				parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond.aspx';
+
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.pageNumber).to.eql(1);
+					done();
+				});
+			});
+
+			it('with page number sets pageNumber', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
+
+				parsedData[1].url_page = '/ks16295577_luxury-hotels_loch-lomond-p2.aspx';
+
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.pageNumber).to.eql(2);
+					done();
+				});
+			});
 		});
 	});
 
-	it('sets search type to region', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
-
-		parsedData[1].url_page = '/r11550_hotels-in-glasgow.aspx';
-
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.type).to.eql('region');
-			done();
-		});
-	});
-
-	it('sets search regionId', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
-
-		parsedData[1].url_page = '/r11550_hotels-in-glasgow.aspx';
-
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.regionId).to.eql(11550);
-			done();
-		});
-	});
-
-	it('sets search two word region', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
-
-		parsedData[1].url_page = '/r11550_hotels-in-new zealand.aspx';
-
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.region).to.eql('new zealand');
-			done();
-		});
-	});
-
-	it('with no page number sets pageNumber to first', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
-
-		parsedData[1].url_page = '/r11550_hotels-in-new zealand.aspx';
-
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.pageNumber).to.eql(1);
-			done();
-		});
-	});
-
-	it('with page number sets pageNumber', function(done) {
-		var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
-		var parsedData = JSON.parse(fileData);
-
-		parsedData[1].url_page = '/r246_hotels-in-lake-district-p2.aspx';
-
-		buildRequest({
-			events: parsedData
-		}).then(function(result) {
-			expect(result[0].search.pageNumber).to.eql(2);
-			done();
-		});
-	});
-
-	describe('quick search region search', function() {
-		it('sets region', function(done) {
+	describe('region', function() {
+		it('sets search region', function(done) {
 			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
 			var parsedData = JSON.parse(fileData);
 
-			parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk.aspx';
+			parsedData[1].url_page = '/r11550_hotels-in-glasgow.aspx';
 
 			buildRequest({
 				events: parsedData
 			}).then(function(result) {
-				expect(result[0].search.region).to.eql('uk');
+				expect(result[0].search.region).to.eql('glasgow');
 				done();
 			});
 		});
 
-		it('sets regionId', function(done) {
+		it('sets search type to region', function(done) {
 			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
 			var parsedData = JSON.parse(fileData);
 
-			parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk.aspx';
+			parsedData[1].url_page = '/r11550_hotels-in-glasgow.aspx';
 
 			buildRequest({
 				events: parsedData
 			}).then(function(result) {
-				expect(result[0].search.regionId).to.eql(100);
+				expect(result[0].search.type).to.eql('region');
 				done();
 			});
 		});
 
-		it('sets quickSearchType', function(done) {
+		it('sets search regionId', function(done) {
 			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
 			var parsedData = JSON.parse(fileData);
 
-			parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk.aspx';
+			parsedData[1].url_page = '/r11550_hotels-in-glasgow.aspx';
 
 			buildRequest({
 				events: parsedData
 			}).then(function(result) {
-				expect(result[0].search.quickSearchType).to.eql('guest houses bed and breakfasts');
+				expect(result[0].search.regionId).to.eql(11550);
 				done();
 			});
 		});
 
-		it('sets isQuickSearch', function(done) {
+		it('sets search two word region', function(done) {
 			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
 			var parsedData = JSON.parse(fileData);
 
-			parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk.aspx';
+			parsedData[1].url_page = '/r11550_hotels-in-new zealand.aspx';
 
 			buildRequest({
 				events: parsedData
 			}).then(function(result) {
-				expect(result[0].search.isQuickSearch).to.eql(true);
+				expect(result[0].search.region).to.eql('new zealand');
 				done();
 			});
 		});
@@ -374,7 +307,7 @@ describe('ratesAccuracyCheck with search', function() {
 			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
 			var parsedData = JSON.parse(fileData);
 
-			parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk.aspx';
+			parsedData[1].url_page = '/r11550_hotels-in-new zealand.aspx';
 
 			buildRequest({
 				events: parsedData
@@ -388,13 +321,99 @@ describe('ratesAccuracyCheck with search', function() {
 			var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
 			var parsedData = JSON.parse(fileData);
 
-			parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk-p2.aspx';
+			parsedData[1].url_page = '/r246_hotels-in-lake-district-p2.aspx';
 
 			buildRequest({
 				events: parsedData
 			}).then(function(result) {
 				expect(result[0].search.pageNumber).to.eql(2);
 				done();
+			});
+		});
+
+		describe('quick search region search', function() {
+			it('sets region', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
+
+				parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk.aspx';
+
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.region).to.eql('uk');
+					done();
+				});
+			});
+
+			it('sets regionId', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
+
+				parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk.aspx';
+
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.regionId).to.eql(100);
+					done();
+				});
+			});
+
+			it('sets quickSearchType', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
+
+				parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk.aspx';
+
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.quickSearchType).to.eql('guest houses bed and breakfasts');
+					done();
+				});
+			});
+
+			it('sets isQuickSearch', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
+
+				parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk.aspx';
+
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.isQuickSearch).to.eql(true);
+					done();
+				});
+			});
+
+			it('with no page number sets pageNumber to first', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
+
+				parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk.aspx';
+
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.pageNumber).to.eql(1);
+					done();
+				});
+			});
+
+			it('with page number sets pageNumber', function(done) {
+				var fileData = fs.readFileSync(__dirname + '/data/rateAccuracyWithLinkedSearch.json');
+				var parsedData = JSON.parse(fileData);
+
+				parsedData[1].url_page = '/rs100_guest-houses-bed-and-breakfasts_uk-p2.aspx';
+
+				buildRequest({
+					events: parsedData
+				}).then(function(result) {
+					expect(result[0].search.pageNumber).to.eql(2);
+					done();
+				});
 			});
 		});
 	});
